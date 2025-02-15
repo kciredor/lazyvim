158c1be6624f89869efc80005012f4ad6606d0d7
Roderick Schaefer <roderick@kciredor.com>
2025-02-15 10:08:22 +0100

    Synced with upstream and adds languages and preliminary AI support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# kciredor's Neovim (LazyVim) config.
+# kciredor's Neovim config.
 
-TODO:
-- Preload everything for Go, Python, etc: https://www.lazyvim.org/configuration/examples
-- Check https://www.youtube.com/watch?v=N93cTbtLCIM
-- Look into https://www.lazyvim.org/extras/vscode
+Based on LazyVim.
+
+
+TODO: Replace LazyExtra ai.supermaven with [CodeCompanion](https://github.com/LazyVim/LazyVim/pull/4268) using Gemini or local model.
---
+++ lua/plugins/languages.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "c",
+        "dockerfile",
+        "go",
+        "helm",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "regex",
+        "rust",
+        "terraform",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
